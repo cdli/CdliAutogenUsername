e6dcb71b345b49311b2dedf977c5dbe8f6c9d436
Adam Lundrigan <adam@lundrigan.ca>
2012-06-06 23:31:52 -0230

    Improved autoloading in tests Bootstrap

--- tests/Bootstrap.php
+++ tests/Bootstrap.php
@@ -1,4 +1,29 @@
 <?php
 
-require_once __DIR__ . '/../../../vendor/autoload.php';
+chdir(__DIR__);
+
+$previousDir = '.';
+while (!file_exists('config/application.config.php')) {
+    $dir = dirname(getcwd());
+    if (file_exists('vendor/autoload.php')) {
+        require_once 'vendor/autoload.php';
+        break;
+    }
+    if($previousDir === $dir) {
+        break;
+    }
+    $previousDir = $dir;
+    chdir($dir);
+}
+
+if (!in_array('vendor/autoload.php',get_included_files())) {
+    defined('ZF2_PATH') || define('ZF2_PATH', getenv('ZF2_PATH') ?: '');
+    require_once ZF2_PATH . 'Zend/Loader/StandardAutoloader.php';
+    $loader = new Zend\Loader\StandardAutoloader(array(
+        'Zend' => ZF2_PATH . 'Zend'
+    ));
+    $loader->register();
+}
+
 require_once __DIR__ . '/../autoload_register.php';
+
