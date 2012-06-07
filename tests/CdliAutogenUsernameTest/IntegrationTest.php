cc5b8d2d121d71448795e0833013dce4277ca0f4
Adam Lundrigan <adamlundrigan@cdli.ca>
2012-06-07 12:04:07 -0230

    Added Integration test to assert that Generator will keep trying until Datasource::isUsernameTaken returns false

--- tests/CdliAutogenUsernameTest/IntegrationTest.php
+++ tests/CdliAutogenUsernameTest/IntegrationTest.php
@@ -41,9 +41,41 @@ class IntegrationTest extends \PHPUnit_Framework_TestCase
         ));
 
         $mock = $this->getMock('CdliAutogenUsername\Datasource\DatasourceInterface');
-        $mock->expects($this->any())->method('isUsernameTaken')->will($this->returnCallback(function($value) {
-            return false;
-        }));
+        $mock->expects($this->any())->method('isUsernameTaken')->will($this->returnCallback(
+            function($value) {
+                return false;
+            }
+        ));
+        $gen->getDatasourceBroker()->register('test', $mock);
+
+        $this->assertRegexp('/^[0-9]{5}TEST$/', $gen->generate());
+    }
+
+    public function testFilterChainsWithDatasourceLookupWhichReturnsTrueOnTheThirdCheck()
+    {
+        $gen = new Generator(array(
+            'datasource' => array(
+                'plugin' => 'test'
+            ),
+            'filters' => array(
+                'digits' => array(
+                    'filter' => 'RandomDigits',
+                    'options' => array( 'digits' => 5 ),
+                ),
+                'prefix' => array(
+                    'filter' => 'StaticString',
+                    'options' => array( 'string' => 'TEST' ),
+                ),
+            ),
+        ));
+
+        $counter = 0;
+        $mock = $this->getMock('CdliAutogenUsername\Datasource\DatasourceInterface');
+        $mock->expects($this->exactly(3))->method('isUsernameTaken')->will($this->returnCallback(
+            function($value) use (&$counter) {
+                return (++$counter<3);
+            }
+        ));
         $gen->getDatasourceBroker()->register('test', $mock);
 
         $this->assertRegexp('/^[0-9]{5}TEST$/', $gen->generate());
