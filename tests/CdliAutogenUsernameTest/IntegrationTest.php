f63f8c4c93e2b2e9051a356df31dec5223664995
Adam Lundrigan <adam@lundrigan.ca>
2012-06-07 00:51:41 -0230

    Add skeleton integration test class

---
+++ tests/CdliAutogenUsernameTest/IntegrationTest.php
@@ -0,0 +1,26 @@
+<?php
+namespace CdliAutogenUsernameTest;
+
+use CdliAutogenUsername\Generator;
+
+class IntegrationTest extends \PHPUnit_Framework_TestCase
+{
+    public function testSingleFilterChain()
+    {
+        $gen = new Generator(array(
+            'datasource' => 'test',
+            'filters' => array(
+                'test' => array(
+                    'filter' => 'randomdigits',
+                    'priority' => -100,
+                    'options' => array( 'digits' => 5 )
+                ),
+            ),
+        ));
+
+        $mock = $this->getMock('CdliAutogenUsername\Datasource\DatasourceInterface');
+        $gen->getDatasourceBroker()->register('test', $mock);
+
+        var_dump($gen->generate());
+    }
+}
