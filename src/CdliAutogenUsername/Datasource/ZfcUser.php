c35e24f07f195db1e39e444306cfd7c6259af07b
Adam Lundrigan <adamlundrigan@cdli.ca>
2012-06-07 12:50:46 -0230

    Flesh out Datasource\ZfcUser

--- src/CdliAutogenUsername/Datasource/ZfcUser.php
+++ src/CdliAutogenUsername/Datasource/ZfcUser.php
@@ -1,7 +1,27 @@
 <?php
 namespace CdliAutogenUsername\Datasource;
 
+use ZfcUser\Model\UserMapper;
+
 class ZfcUser implements DatasourceInterface
 {
+    protected $mapper;
 
+    public function isUsernameTaken($username)
+    {
+        return is_object($this->getMapper()->findByUsername($username));
+    }
+
+    public function setOptions($options) {}
+
+    public function setMapper(UserMapper $mapper)
+    {
+        $this->mapper = $mapper;
+        return $this;
+    }
+
+    public function getMapper()
+    {
+        return $this->mapper;
+    }
 }
