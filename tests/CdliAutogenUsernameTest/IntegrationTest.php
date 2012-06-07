3f3af195658f2a4219ec5a7aed4407b74cf3c5b9
Adam Lundrigan <adamlundrigan@cdli.ca>
2012-06-07 11:52:28 -0230

    Add datasource lookup to the integration test

--- tests/CdliAutogenUsernameTest/IntegrationTest.php
+++ tests/CdliAutogenUsernameTest/IntegrationTest.php
@@ -21,4 +21,32 @@ class IntegrationTest extends \PHPUnit_Framework_TestCase
         ));
         $this->assertRegexp('/^[0-9]{5}TEST$/', $gen->generate());
     }
+
+    public function testFilterChainsWithDatasourceLookupWhichNeverReturnsTrue()
+    {
+        $gen = new Generator(array(
+            'datasource' => array(
+                'plugin' => 'test'
+            ),
+            'filters' => array(
+                'digits' => array(
+                    'filter' => 'RandomDigits',
+                    'options' => array( 'digits' => 5 ),
+                ),
+                'prefix' => array(
+                    'filter' => 'StaticString',
+                    'options' => array( 'string' => 'TEST' ),
+                ),
+            ),
+        ));
+
+        $mock = $this->getMock('CdliAutogenUsername\Datasource\DatasourceInterface');
+        $mock->expects($this->any())->method('isUsernameTaken')->will($this->returnCallback(function($value) {
+            return false;
+        }));
+        $gen->getDatasourceBroker()->register('test', $mock);
+
+        $this->assertRegexp('/^[0-9]{5}TEST$/', $gen->generate());
+    }
+
 }
